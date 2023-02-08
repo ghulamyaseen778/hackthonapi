f224ba5dbd2a4b0d4be48580fa5e37b414302914
ghulamyaseen778 <muhammadyaseen3294@gmail.com>
2023-02-08 23:14:47 +0500

    ascyn

--- index.js
+++ index.js
@@ -6,14 +6,21 @@ const app = express() //initilaze express
 const PORT = process.env.PORT || 5000 //define server port
 const route = require('./routes')
 // //make mongodb connection using mongoose
-mongoose.connect(process.env.MONGO_DB_URI)
-.then((res)=>console.log('server Connceted')) //response catching
-.catch((err)=>console.log(err)) //error catching
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 app.use(express.json()) //intilaize express in json
 app.use(cors()) //intilaze cors because server or api use in react app
 app.use('/api',route)
 
-app.listen(PORT, () => {
+connectDB().then(() => {
+  app.listen(PORT, () => {
     console.log(`Example app listening on port http://localhost:${PORT}`)
   })
+})
