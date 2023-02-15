49b59bcfd33131991561eb271ab71eab80dacbfa
ghulamyaseen778 <muhammadyaseen3294@gmail.com>
2023-02-15 12:50:56 +0500

    bug-fixing

--- database/registration.js
+++ database/registration.js
@@ -4,28 +4,50 @@ const RequestAndResponseHandler = require('../helper')
 
 const createSignUpData = (queryObject, res) => {
   try {
-    UserSchema.create(queryObject, (err, data) => {
-      if (err) {
-        RequestAndResponseHandler( "", res,MsgFunc.msg1)//check error
-      } else {
-        RequestAndResponseHandler(data, res)//check data and send response
-      }
-    })
+   UserSchema.findOne({email:queryObject.email},(err,data)=>{
+    if(data!=null){
+      RequestAndResponseHandler( "", res,MsgFunc.msg1)//check error
+    }
+    else{
+      UserSchema.create(queryObject, (err, data) => {
+        if (!data) {
+          RequestAndResponseHandler( "", res,MsgFunc.msg1)//check error
+        } else {
+          const {user_name,email,password,mobile_no,joining,_id} = data
+          RequestAndResponseHandler({
+            token:_id,
+            userName:user_name,
+            email,
+            joining,
+            mobileNumber:mobile_no
+          }, res)//check data and send response
+        }
+      })
+    }
+   })
   } catch (error) {
     RequestAndResponseHandler("", res,MsgFunc.msg1)
   }
 }
 const LoginData = ({ email,password }, res) => {
   try {
-    UserSchema.findOne({ email }, (err) => {
-      if (err) {
+    UserSchema.findOne({ email }, (err,data) => {
+      // console.log(err,"err")
+      if (!data) {
         RequestAndResponseHandler( "", res,MsgFunc.msg1)//check error
       } else {
         UserSchema.findOne({password}, (err, data) => {
-          if (err) {
+          if (!data) {
             RequestAndResponseHandler("", res,MsgFunc.msg1)//check error
           } else {
-            RequestAndResponseHandler( data, res)//check data and send response
+            const {user_name,email,password,mobile_no,joining,_id} = data
+          RequestAndResponseHandler({
+            token:_id,
+            userName:user_name,
+            email,
+            joining,
+            mobileNumber:mobile_no
+          }, res)//check data and send response //check data and send response
           }
         })
       }
